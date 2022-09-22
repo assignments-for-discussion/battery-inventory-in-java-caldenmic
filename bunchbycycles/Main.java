9b84d2b3bb577196a189092a981cb24e5156e977
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2022-09-22 17:38:53 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -9,12 +9,24 @@ public class Main {
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
+    for(int i=0; i<cycles.length; i++) {
+      if(cycles[i] < 410) {
+        counts.lowCount += 1;
+      }
+      else if(cycles[i] >= 410 && cycles[i] <=909) {
+        counts.mediumCount += 1;
+      }
+      else {
+        counts.highCount += 1;
+      }
+    }
     return counts;
   }
 
   static void testBucketingByNumberOfCycles() {
     System.out.println("Counting batteries by usage cycles...\n");
     CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
+//     System.out.println(counts.lowCount + " " + counts.mediumCount + " " + counts.highCount);
     assert(counts.lowCount == 2);
     assert(counts.mediumCount == 3);
     assert(counts.highCount == 1);
